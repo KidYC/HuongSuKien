abd2b48a12b04e8fde25a0991f57562ef8c4e45f
Minh-PC <Minh-PC@DESKTOP-JK489Q9>
2026-05-06 16:08:40 +0700

    clas Ban

---
+++ Class/Ban.java
@@ -0,0 +1,36 @@
+package Class;
+
+public class Ban {
+	private int maBan;
+	private String tenBan;
+	private String trangThai;
+	public Ban(int maBan, String tenBan, String trangThai) {
+		this.maBan = maBan;
+		this.tenBan = tenBan;
+		this.trangThai = trangThai;
+	}
+	public int getMaBan() {
+		return maBan;
+	}
+	public void setMaBan(int maBan) {
+		this.maBan = maBan;
+	}
+	public String getTenBan() {
+		return tenBan;
+	}
+	public void setTenBan(String tenBan) {
+		this.tenBan = tenBan;
+	}
+	public String getTrangThai() {
+		return trangThai;
+	}
+	public void setTrangThai(String trangThai) {
+		this.trangThai = trangThai;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return maBan + "-"+tenBan+"-"+trangThai;
+	}
+    
+}
